cad72550e5022e7c428dc14c28a9419d76e2e18a
Sabrina Fraschetti <sabrinafraschetti@Sabrinas-MacBook-Pro.local>
2024-11-27 17:05:23 +0000

    completed task 13-get/api/articles/article_id-commentCount

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,7 +1,11 @@
 const db = require("../db/connection")
 
 exports.fetchArticleById = (article_id) => {
-    return db.query('SELECT * FROM articles WHERE article_id = $1;', [article_id])
+    return db.query(`SELECT articles.*, COUNT(comments.comment_id) AS comment_count 
+      FROM articles 
+      LEFT JOIN comments ON articles.article_id = comments.article_id
+      WHERE articles.article_id = $1
+      GROUP BY articles.article_id;`, [article_id])
     .then(({ rows }) => {
         if (rows.length === 0){
             return Promise.reject({ status: 404, msg: 'article does not exist' })
@@ -51,9 +55,6 @@ exports.fetchArticles = (author, topic, sort_by = "created_at", order = "desc") 
   ORDER BY ${sort_by} ${order}; `
 
   return db.query(sqlQuery, queryValues).then(({ rows }) => {
-    // if (rows.length === 0) {
-    //   return Promise.reject({ status: 404, msg: `not found` })
-    // }
     return rows
   });
 };
