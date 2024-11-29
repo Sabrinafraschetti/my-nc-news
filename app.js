067c0f66ac7824b715a23b39e4c8cc88570c04f4
Sabrina Fraschetti <sabrinafraschetti@Sabrinas-MacBook-Pro.local>
2024-11-29 10:54:39 +0000

    completed task 16-add-express-router

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
 const express = require("express")
-const { getApi } = require("./controllers/api.controller")
-const { getTopics } = require("./controllers/topics.controller")
-const { getArticleById, getArticles, patchArticleVotesById } = require("./controllers/articles.controller")
 const { psqlErrorHandler, customErrorHandler, serverErrorHandler } = require("./errorHandling")
-const { getCommentsByArticleId, postCommentById, deleteCommentById } = require("./controllers/comments.controller")
-const { getUsers } = require("./controllers/users.controller")
 const app = express()
+const apiRouter = require("./routes/api-router")
 
 app.use(express.json());
 
-app.get("/api", getApi)
-
-app.get("/api/topics", getTopics)
-
-app.get("/api/articles/:article_id", getArticleById)
-
-app.get("/api/articles", getArticles)
-
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId)
-
-app.post("/api/articles/:article_id/comments", postCommentById)
-
-app.patch("/api/articles/:article_id", patchArticleVotesById)
-
-app.delete("/api/comments/:comment_id", deleteCommentById)
-
-app.get("/api/users", getUsers)
+app.use("/api", apiRouter);
 
 app.use('/*', (req, res) => {
     res.status(404).send({ msg: 'Route not found' })
